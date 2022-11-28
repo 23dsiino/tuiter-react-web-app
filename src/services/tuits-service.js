baeb41adcef6a777d8c0014f9892cfde7599135a
danielleSiino23 <siino.d@northeastern.edu>
2022-11-28 14:08:33 -0500

    a9

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -12,12 +12,11 @@ export const deleteTuit = async (tid) => {
         .delete(`${TUITS_API}/$tid`)
     return response.data
 }
+
 export const updateTuit = async (tuit) => {
-    const response = await axios
-        .put(`${TUITS_API}/${tuit._id}`, tuit);
+    const response = await axios.put(`${TUITS_API}/${tuit._id}`, tuit);
     return tuit;
 }
-
 
 export const findTuits = async () => {
     const response = await axios.get(TUITS_API);
